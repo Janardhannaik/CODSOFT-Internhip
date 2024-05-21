8921abde058691de3e6cdca5f0234dbdd10ec251
JANARDHAN NAIK <naikjanardhan568@gmail.com>
2024-05-21 12:16:16 +0530

    first commit

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../Css/Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer"></div>
+      <div className="copyright">
+        <p className="copyright-blog">
+          Janardhan Naik Intern At Codsoft Intern Program
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
